27f0eb06617ab3be925edcdd4acceda968cd54ca
Julio Dutra <jjcdutra@gmail.com>
2021-09-16 20:28:34 -0300

    Organizando a estrutura do projeto

---
+++ src/main/kotlin/com/github/jjcdutra/avengers/avengers/web/request/AvengerRequest.kt
@@ -0,0 +1,21 @@
+package com.github.jjcdutra.avengers.avengers.web.request
+
+import javax.validation.constraints.NotBlank
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
+
+data class AvengerRequest(
+
+    @field:NotNull
+    @field:NotBlank
+    @field:NotEmpty
+    val nick: String,
+
+    @field:NotNull
+    @field:NotBlank
+    @field:NotEmpty
+    val person: String,
+
+    val description: String? = "",
+    val history: String? = "",
+)
---
+++ src/main/kotlin/com/github/jjcdutra/avengers/avengers/web/response/AvengerResponse.kt
@@ -0,0 +1,8 @@
+package com.github.jjcdutra.avengers.avengers.web.response
+
+data class AvengerResponse(
+    val nick: String,
+    val person: String,
+    val description: String,
+    val history: String,
+)
