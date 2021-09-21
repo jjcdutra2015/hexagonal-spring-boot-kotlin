88b7574a52514df93e6f2735594815a4f026c185
Julio Dutra <jjcdutra@gmail.com>
2021-09-21 20:27:42 -0300

    Conceito porta e adaptador

--- src/main/kotlin/com/github/jjcdutra/avengers/avengers/resource/avenger/AvengerRepositoryImpl.kt
+++ src/main/kotlin/com/github/jjcdutra/avengers/avengers/resource/avenger/AvengerRepositoryImpl.kt
@@ -1,12 +1,27 @@
 package com.github.jjcdutra.avengers.avengers.resource.avenger
 
+import com.github.jjcdutra.avengers.avengers.domain.avenger.Avenger
+import com.github.jjcdutra.avengers.avengers.domain.avenger.AvengerRepository
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Component
 
 @Component
 class AvengerRepositoryImpl(
     @Autowired private val repository: AvengerEntityRepository
-) {
+) : AvengerRepository {
 
+    override fun getDetail(id: Long): Avenger? =
+        repository.findByIdOrNull(id)?.toAvenger()
 
+    override fun getAvengers(): List<Avenger> =
+        repository.findAll().map { it.toAvenger() }
+
+    override fun create(avenger: Avenger): Avenger =
+        repository.save(AvengerEntity.from(avenger)).toAvenger()
+
+    override fun delete(id: Long) = repository.deleteById(id)
+
+    override fun update(avenger: Avenger): Avenger =
+        repository.save(AvengerEntity.from(avenger)).toAvenger()
 }
